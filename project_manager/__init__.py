b50f3769cf51e4b54dd566ef5ca61121a4e22ccb
JD <10216562+joeld1@users.noreply.github.com>
2021-04-18 06:15:53 -0700

    style:added alias namespaces for __init__.py

--- project_manager/__init__.py
+++ project_manager/__init__.py
@@ -1,24 +1,29 @@
 __version__ = '0.1.0'
 
-from project_manager.project_manager import ProjectManager, PoetryProjectManager, CommonPSCommands, CondaEnvManager, \
-    GitProjectManager, LocalProjectManager, SublimeBuildConfigGenerator
+from project_manager.project_manager import ProjectManager as PM
+from project_manager.project_manager import PoetryProjectManager as PPM
+from project_manager.project_manager import CommonPSCommands as CPSC
+from project_manager.project_manager import CondaEnvManager as CEM
+from project_manager.project_manager import GitProjectManager as GPM
+from project_manager.project_manager import LocalProjectManager as LPM
+from project_manager.project_manager import SublimeBuildConfigGenerator as SBCG
 
 # Include these here to lazily access static methods
 
 
 
 # Sets up Ipykernel and registers conda kernel name for a previously made conda env
-init_prev_made_conda_env = CondaEnvManager.init_prev_made_conda_env
+init_prev_made_conda_env = CEM.init_prev_made_conda_env
 
 # creates, inits, and registers a conda env and kernel
-create_and_init_conda_env = CondaEnvManager.create_and_init_conda_env
+create_and_init_conda_env = CEM.create_and_init_conda_env
 
 # same as poetry init
-execute_poetry_init = PoetryProjectManager.execute_poetry_init
+execute_poetry_init = PPM.execute_poetry_init
 
 # creates a poetry.toml and pyproject.toml in same directory
-link_poetry_proj_with_conda_env = PoetryProjectManager.link_poetry_proj_with_conda_env
+link_poetry_proj_with_conda_env = PPM.link_poetry_proj_with_conda_env
 
-init_curent_dir_as_a_poetry_conda_project = LocalProjectManager.init_curent_dir_as_a_poetry_conda_project
+init_curent_dir_as_a_poetry_conda_project = LPM.init_curent_dir_as_a_poetry_conda_project
 
-generate_sublime_text_3_build_config_from_conda_env = SublimeBuildConfigGenerator.generate_sublime_text_3_build_config_from_conda_env
+generate_sublime_text_3_build_config_from_conda_env = SBCG.generate_sublime_text_3_build_config_from_conda_env
